8d805cc7473f3cf0ff609856e8a5c5add9a58fe1
Vrushabh Damle <dwrushabh@gmail.com>
2025-11-08 11:26:52 +0530

    A crappy frontend is ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,61 @@
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="bg-red-700">Hi there</div>;
-}
+  const [messages, setMessages] = useState(["hi there", "hello"]);
 
+  const wsRef = useRef<WebSocket | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current = ws;
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+    return () => {
+      ws.close;
+    };
+  }, []);
+  return (
+    <div className="h-screen bg-neutral-950 flex flex-col justify-between">
+      <div className="mt-8 f">
+        {messages.map((message) => (
+          <div className="m-10">
+            <span className="bg-cyan-100 p-4 m-8 rounded">{message} </span>
+          </div>
+        ))}
+      </div>
+      <div className="bg-neutral-300 w-full flex ">
+        {" "}
+        <input ref={inputRef} className="flex-1 p-4 outline-none border-none" />
+        <button
+          className="bg-green-600 text-neutral-50 p-4 cursor-pointer outline-none border-none"
+          onClick={() => {
+            const message = inputRef.current?.value;
+            wsRef?.current?.send(
+              JSON.stringify({
+                type: "chat",
+                payload: {
+                  message: message,
+                },
+              })
+            );
+          }}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
 export default App;
